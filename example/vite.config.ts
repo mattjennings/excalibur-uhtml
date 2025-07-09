03e13fcbfdaf8de13ece1d15bfd4fe9d1f689219
Matt Jennings <me@mattjennings.io>
2025-07-08 22:21:09 -0500

    pages

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.CI ? '/excalibur-uhtml/' : '/',
   resolve: {
     alias: {
       'excalibur-uhtml': path.resolve(__dirname, '../src/index.ts'),
